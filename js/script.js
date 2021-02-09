bbd7e5c8e095584147d6ccb8fc320222b95444d2
testtsug <78212841+testtsug@users.noreply.github.com>
2021-02-09 18:28:30 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -206,7 +206,7 @@ $(function () {
             $b.removeClass('open');
         } else {
             $b.addClass('open');
-        }   
+        }
 
 
         // テキストをセット
@@ -238,33 +238,136 @@ $(function () {
     });
 
 
+
+
+
+
+
+
+
+    //リセットボタン
+    $('.reset').click(function () {
+        console.log('.reset');
+        // 合計金額の計算
+        $kingaku = 0; // 合計金額
+        $kosuu = 0; // 合計個数
+        $null = true; // 入力有無
+
+        // ボタンの分だけループで一括処理
+        // ボタンの親要素を対象にして金額の合計を計算する
+        $('input[type="number"]').each(function (index, elm) {
+
+            // 計算対象
+            $dl = $(this).parent().parent();
+
+            // 商品名
+            $dt = $dl.find('dt').text();
+            // 金額
+            $label = $dl.find('label').text();
+            // 金額に余計な文字が合ったら消す。今回は”円”
+            $label = $label.replace('円', '');
+
+            // 個数
+            // 数字型に変換しておく
+            $(this).val(0);
+            $kazu = Number($(this).val());
+
+            // 合計金額に加算、合計個数に加算
+            $kingaku += ($label * $kazu);
+            $kosuu += $kazu;
+
+            //box2 class_change
+            //変更後の文字列
+            $res = $dt + "/" + $(this).val() + "個：" + ($label * $(this).val()) + "円";
+            // 対象指定
+            $target = $('#box2 p:nth-of-type(' + (index + 1) + ')')
+            $target.text($res);
+            if ($(this).val() == 0) {
+                $target.removeClass('open');
+            } else {
+                $target.addClass('open');
+                $null = false;
+                if (($label * $kazu) >= 0) {
+                    $target.css('color', 'Black');
+                } else {
+                    $target.css('color', 'Red');
+                }
+            }
+
+            // カートの上のバッジ
+            $bi = $('.badge i');
+            $bi.attr('data-badge', $kosuu);
+        });
+
+        // カートの上のやつを消すやつ
+        $b = $('.badge');
+        if (!$kosuu) {
+            $b.removeClass('open');
+        } else {
+            $b.addClass('open');
+        }
+
+
+        // テキストをセット
+        $('#total').text("合計：" + $kingaku + "円");
+
+        // 合計金額がマイナスなら赤字
+        if ($kingaku >= 0) {
+            $('#total').css('color', 'Black');
+        } else {
+            $('#total').css('color', 'Red');
+        }
+
+        // box2 class_change
+        if ($null) {
+            $('#box2 p:last-of-type').removeClass('open');
+        } else {
+            $('#box2 p:last-of-type').addClass('open');
+            $res = "合計/" + $kosuu + "個：" + $kingaku + "円";
+            $('#box2 p:last-of-type').text($res);
+        }
+        // #box2に.openがないなら#box2がどっかいく
+        if ($('#box2 .open').length) {
+            $('#box2').addClass('open');
+        } else {
+            $('#box2').removeClass('open');
+        }
+
     });
 
 
-    $(function () {
-        $('input[type="number"]').change(function () {
-            let controls = $('input[type="number"]'); //$(this)だと1つしか持ってこれない
-            let controlobj = {};
-            let purchaseNumber = 0; //カートアイコンに表示するバッジの数字
-
-            for (let i = 0; i < controls.length; i++) {
-                controlobj[controls[i].name] = $('input[name="' + controls[i].name + '"]').val();
-                purchaseNumber += parseInt($('input[name="' + controls[i].name + '"]').val());
-            }
-            $('#view').load(
-                'php/cart.php',
-                controlobj,
-                function (data, textStatus, XMLHttpRequest) {}
-            );
-
-            $('a.badge').attr('data-badge', purchaseNumber); //カートボタンのバッジの購入数を書き換える
-            $('input.badge').attr('value', purchaseNumber);
-        });
-        $('input[type="reset"]').click(function () {
-            $('#view').empty();
-        });
 
 
-        
+
+
+
+});
+
+
+$(function () {
+    $('input[type="number"]').change(function () {
+        let controls = $('input[type="number"]'); //$(this)だと1つしか持ってこれない
+        let controlobj = {};
+        let purchaseNumber = 0; //カートアイコンに表示するバッジの数字
+
+        for (let i = 0; i < controls.length; i++) {
+            controlobj[controls[i].name] = $('input[name="' + controls[i].name + '"]').val();
+            purchaseNumber += parseInt($('input[name="' + controls[i].name + '"]').val());
+        }
+        $('#view').load(
+            'php/cart.php',
+            controlobj,
+            function (data, textStatus, XMLHttpRequest) {}
+        );
+
+        $('a.badge').attr('data-badge', purchaseNumber); //カートボタンのバッジの購入数を書き換える
+        $('input.badge').attr('value', purchaseNumber);
+    });
+    $('input[type="reset"]').click(function () {
+        $('#view').empty();
+    });
+
+
+
 
 });
